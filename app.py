3b9351bca4f127324defaf0eefdb0055b59886b8
Mickael PAPAMICA Asseline <mickael@papamica.com>
2023-07-18 00:14:24 +0200

    🐛 fix(app.py): fix indentation and remove unnecessary blank lines ✨ feat(app.py): add support for Uptime Kuma API to fetch monitors, tags, and status pages ✨ feat(app.py): add support for adding a new status page if it doesn't exist ✨ feat(app.py): add support for adding tags if they don't exist ✨ feat(app.py): add support for adding monitors if they don't exist ✨ feat(app.py): add support for adding tags to monitors ✨ feat(app.py): add support for creating public group list for status page ✨ feat(app.py): add support for saving status page with public group list ✨ feat(app.py): add support for posting incidents to status page 🔧 chore(app.py): remove commented out code and unused variables

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import requests
 import json
 import os
+from uptime_kuma_api import UptimeKumaApi
+from uptime_kuma_api import MonitorType,IncidentStyle
+
+
+
 
 def zabbix_login(url, username, password):
     """
@@ -58,25 +63,164 @@ zabbix_password = os.environ.get('ZABBIX_PASSWORD')
 
 auth_token = zabbix_login(zabbix_url, zabbix_username, zabbix_password)
 status = zabbix_get_status(zabbix_url, auth_token)
+#print(status)
 
-for item in status:
-    #print(dict(item))
-    status_text = ""
-    if item['status'] == "-1":
-        status_text = "OK"
-    elif item['status'] == "0":
-        status_text = "Non classé"
-    elif item['status'] == "1":
-        status_text = "Information"
-    elif item['status'] == "2":
-        status_text = "Avertissement"
-    elif item['status'] == "3":
-        status_text = "Moyen"
-    elif item['status'] == "4":
-        status_text = "Haut"
-    elif item['status'] == "5":
-        status_text = "Urgent"
-    
-    print(f"\n\nNom: {item['name']}\n → Statut: {status_text}")
-    if 'problem_events' in item and len(item['problem_events']) > 0:
-        print(f" → Problème: {item['problem_events'][0]['name']}")
+monitors = []
+statuspages = []
+tags = []
+with UptimeKumaApi(os.environ.get('KUMA_URL')) as api:
+    api.login(os.environ.get('KUMA_USERNAME'), os.environ.get('KUMA_PASSWORD'))
+    monitors_list = api.get_monitors()
+    for monitor in monitors_list:
+        monitors.append({
+                "id": monitor['id'],
+                "name": monitor['name']
+            })
+    tag_list = api.get_tags()
+    for tag in tag_list:
+        tags.append({
+                "id": tag['id'],
+                "name": tag['name']
+            })
+    statuspages_list = api.get_status_pages()
+    for statuspage in statuspages_list:
+        statuspages.append(statuspage['slug'])
+
+    if 'zbx2kuma' not in statuspages:
+        api.add_status_page("zbx2kuma", "zbx2kuma")
+        print(f"Statuspage zbx2kuma Added !")
+    else:
+        print(f"Statuspage zbx2kuma already exist !")
+
+monitors_id_list = []
+events_list = []
+categories = []
+
+
+
+
+with UptimeKumaApi(os.environ.get('KUMA_URL')) as api:
+    api.login(os.environ.get('KUMA_USERNAME'), os.environ.get('KUMA_PASSWORD'))
+    for item in status:
+        if 'parents' in item and len(item['parents']) > 0:
+            for parent in item['parents']:
+                if parent['name'] not in [tag['name'] for tag in tags]:
+                    tag_added = api.add_tag(
+                        name=parent['name'],
+                        color="#0098FF"
+                    )
+                    tags.append({
+                        "id": tag_added['id'],
+                        "name": tag_added['name']
+                    })
+                    print(f"Tag {parent['name']} Added !")
+                else:
+                    print(f"Tag {parent['name']} already exist !")
+
+with UptimeKumaApi(os.environ.get('KUMA_URL')) as api:
+    api.login(os.environ.get('KUMA_USERNAME'), os.environ.get('KUMA_PASSWORD'))
+    for item in status:
+        #print(dict(item))
+        status_text = ""
+        if item['status'] == "-1":
+            status_text = "OK"
+        elif item['status'] == "0":
+            status_text = "Non classé"
+        elif item['status'] == "1":
+            status_text = "Information"
+        elif item['status'] == "2":
+            status_text = "Avertissement"
+        elif item['status'] == "3":
+            status_text = "Moyen"
+        elif item['status'] == "4":
+            status_text = "Haut"
+        elif item['status'] == "5":
+            status_text = "Urgent"
+        
+        
+        
+        if 'parents' in item and len(item['parents']) > 0:  # Edited line
+            print(f"\n\nNom: {item['name']}\n → Statut: {status_text}")
+            if 'problem_events' in item and len(item['problem_events']) > 0:
+                for event in item['problem_events']:
+                    print(f" → Event ID: {event['eventid']} - {event['name']}")
+                    events_list.append({
+                        "source": item['name'],
+                        "name": event['name'],
+                        "eventid": event['eventid']
+                    })
+                
+        
+        
+
+        if 'parents' in item and len(item['parents']) > 0:
+            if item['name'] not in [monitor['name'] for monitor in monitors]:
+                monitor_added = api.add_monitor(
+                    type=MonitorType.HTTP,
+                    name=item['name'],
+                    #tag=item['parents'][0]['name'],
+                    url='https://traefik-' + item['name'] + '.papamica.net'
+                )
+
+                for parent in item['parents']:
+                    parent_tag = [tag['id'] for tag in tags if tag['name'] == parent['name']]
+                    if parent_tag:
+                        api.add_monitor_tag(
+                            tag_id=parent_tag[0],
+                            monitor_id=monitor_added['monitorID']
+                        )
+                print(f"Monitor {item['name']} Added !")
+            else:
+                print(f"Monitor {item['name']} already exist !")
+
+publicGroupList = []
+monitors_list = []
+monitors = []
+monitors_list = api.get_monitors()
+for monitor in monitors_list:
+    #print(monitor)
+    monitors.append({
+            "id": monitor['id'],
+            "name": monitor['name'],
+            "tag": monitor['tags'][0]['name']
+        })
+i=0
+for category in tags:
+    monitors_with_category = [monitor['id'] for monitor in monitors if monitor['tag'] == category['name']]
+    monitors_id_list = monitors_with_category
+    i=i+1
+    publicGroupList.append({
+        'name': category['name'],
+        'weight': i,
+        'monitorList': monitors_id_list
+    })
+
+print(publicGroupList)
+
+with UptimeKumaApi(os.environ.get('KUMA_URL')) as api:
+    api.login(os.environ.get('KUMA_USERNAME'), os.environ.get('KUMA_PASSWORD'))
+    api.save_status_page(
+        slug="zbx2kuma",
+        title="zbx2kuma",
+        description="Page de status automatique avec Zabbix",
+        publicGroupList=publicGroupList
+    )
+    api.disconnect()
+    content = ""
+    if events_list:
+        for event in events_list:
+            content = content + "\n\n**" +  event['source'] + "** → " + event['name']
+    # #print(f"{event['eventid']} - {event['name']}")
+    # with UptimeKumaApi(os.environ.get('KUMA_URL')) as api:
+    #     api.login(os.environ.get('KUMA_USERNAME'), os.environ.get('KUMA_PASSWORD'))
+        api.post_incident(
+            slug="zbx2kuma",
+            title="Incidents en cours",
+            content=content,
+            style=IncidentStyle.WARNING
+        )
+        api.disconnect()
+
+
+
+
